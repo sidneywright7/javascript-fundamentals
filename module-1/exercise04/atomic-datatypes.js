2a07eff42574df11ad7ef824b379949338999a56
Sidney Wright <sidneywright7@gmail.com>
2025-12-13 13:23:14 -0600

    exercise04-atomicdatatypes

---
+++ module-1/exercise04/atomic-datatypes.js
@@ -0,0 +1,7 @@
+console.log(5, typeof 5);
+console.log(5.2, typeof 5.2);
+console.log('text', typeof 'text');
+console.log(true, typeof true); 
+console.log(false, typeof false);
+console.log(undefined, typeof undefined);
+console.log(null, typeof null);
