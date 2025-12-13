3d77a1e6bdf4005fa69030c0dc3a0d46d5af9975
Sidney Wright <sidneywright7@gmail.com>
2025-12-13 13:02:28 -0600

    exercise03

---
+++ module-1/exericse03/index.js
@@ -0,0 +1,7 @@
+let firstvalue = Number(prompt("Enter the first value:"))
+let secondvalue = Number(prompt("Enter the second value:"))
+
+let result = firstvalue * secondvalue; 
+
+console.log(result)
+alert(result) 
