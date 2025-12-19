914550fc68e1aa3fb46c0ab8a537aa5c394429a2
sidneywright7 <sidneywright7@gmail.com>
2025-12-18 19:22:15 -0600

    operators

---
+++ logical-operators.js
@@ -0,0 +1,11 @@
+console.log(false && false --> ;, false && false);
+console.log(false && true --> ;, false && true);
+console.log(true && false --> ;, true && false);        
+console.log(true && true --> ;, true && true);
+
+console.log(logical or operator);
+console.log(false || false --> ;, false || false);
+console.log(false || true --> ;, false || true);
+console.log(true || false --> ;, true || false);        
+console.log(true || true --> ;, true || true);  
+
