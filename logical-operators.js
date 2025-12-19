b056b26cd936819dfacfd99bbc902149d12b9ac4
sidneywright7 <sidneywright7@gmail.com>
2025-12-18 19:27:19 -0600

    operations

--- logical-operators.js
+++ logical-operators.js
@@ -9,3 +9,14 @@ console.log(false || true --> ;, false || true);
 console.log(true || false --> ;, true || false);        
 console.log(true || true --> ;, true || true);  
 
+console.log(negation operator);
+console.log(!false --> ;, !false);
+console.log(!true --> ;, !true);    
+
+console.log(exclusive or operation
+    let a = false;
+    let b = false;
+    let xor= (!a && b) || (a && !b);
+    let axorb = (!a && !b) || (!a && b!);
+    console.log(a XOR b, ;, axorb));
+);
