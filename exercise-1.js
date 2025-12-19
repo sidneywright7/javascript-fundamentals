d45daf911928aee3aed7015a791cc6e79cc3601f
sidneywright7 <sidneywright7@gmail.com>
2025-12-18 20:12:24 -0600

    exercise1

---
+++ exercise-1.js
@@ -0,0 +1,29 @@
+// 1. Console log all numbers that are even between 1 and 25. For odd numbers, console log "odd".
+// a) from 1 to 9 (inclusive)
+// b) from 1 to 10 (inclusive) 
+// c) from 9 to 0 (inclusive)
+// d) from 10 to 1 (inclusive)
+
+// a)
+for (let i = 0; i <= 10; i++) {
+        console.log(i);
+    } 
+
+// b)
+for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    } 
+// c)
+for (let i = 9; i >= 0; i--) {
+        console.log(i);
+    }
+// d)
+for (let i = 10; i >= 1; i--) {
+        console.log(i);
+    }   
+for (let i) 1; i <= 25; i++) {
+    if (i % 2 === 1) {
+        console.log(i);
+    } else {
+        console.log("odd");
+    }   
